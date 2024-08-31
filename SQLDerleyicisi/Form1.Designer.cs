ed0b2e191330248f41c9b108bba919eabe339d0b
Semiha OZCAN <169284658+SemihaOZCAN@users.noreply.github.com>
2024-08-31 17:53:44 +0300

    SQL Derleyici

---
+++ SQLDerleyicisi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SQLDerleyicisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSQLHAKKINDA = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 298);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(596, 290);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(604, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ÇALIŞTIR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonEXECUTE_Click);
+            // 
+            // buttonSQLHAKKINDA
+            // 
+            this.buttonSQLHAKKINDA.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonSQLHAKKINDA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSQLHAKKINDA.Location = new System.Drawing.Point(604, 134);
+            this.buttonSQLHAKKINDA.Name = "buttonSQLHAKKINDA";
+            this.buttonSQLHAKKINDA.Size = new System.Drawing.Size(174, 68);
+            this.buttonSQLHAKKINDA.TabIndex = 3;
+            this.buttonSQLHAKKINDA.Text = "SQL HAKKINDA";
+            this.buttonSQLHAKKINDA.UseVisualStyleBackColor = false;
+            this.buttonSQLHAKKINDA.Click += new System.EventHandler(this.buttonSQLHAKKINDA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSQLHAKKINDA);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Derleyici";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSQLHAKKINDA;
+    }
+}
+
---
+++ SQLDerleyicisi/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLDerleyicisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti=new SqlConnection(@"Data Source=DESKTOP-KIMUOA0\SQLEXPRESS;Initial Catalog=DbOtelRezervasyon;Integrated Security=True");
+
+        private void buttonEXECUTE_Click(object sender, EventArgs e)
+        {
+            string sorgu;
+            sorgu = richTextBox1.Text;
+            try
+            {
+                SqlDataAdapter dr = new SqlDataAdapter(sorgu, baglanti);
+                DataTable dt = new DataTable();
+                dr.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("sorgunuzu tekrar kontrol edin ", "Uyarı", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            }
+
+           
+
+        }
+
+        private void buttonSQLHAKKINDA_Click(object sender, EventArgs e)
+        {
+            //sql kaynak onerme
+            System.Diagnostics.Process.Start("https://www.w3schools.com/sql/");
+        }
+    }
+}
